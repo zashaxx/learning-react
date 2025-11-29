342202fd90abfe95038b554da20ea80a300251b1
zashaxx <hisenberg2248@gmail.com>
2025-11-29 19:52:28 +0545

    understanding react states

--- 04-steps/src/App.js
+++ 04-steps/src/App.js
@@ -8,33 +8,42 @@ const messages = [
 
 export default function App() {
   function handlePrevious() {
-    if (step > 1) setStep(step - 1);
+    if (step > 1) setStep((s) => s - 1);
   }
 
   function handleNext() {
-    if (step < 3) setStep(step + 1);
+    if (step < 3) setStep((s) => s + 1);
   }
   const [step, setStep] = useState(1);
 
-  return (
-    <div className="steps">
-      <div className="numbers">
-        <div className={step >= 1 ? "active" : ""}>1</div>
-        <div className={step >= 2 ? "active" : ""}>2</div>
-        <div className={step >= 3 ? "active" : ""}>3</div>
-      </div>
+  const [isOpen, setIsOpen] = useState(true);
 
-      <p className="message">
-        Step {step}: {messages[step - 1]}{" "}
-      </p>
-      <div className="buttons">
-        <button className="prev" onClick={handlePrevious}>
-          Previous
-        </button>
-        <button className="next" onClick={handleNext}>
-          Next
-        </button>
-      </div>
-    </div>
+  return (
+    <>
+      <button className="close" onClick={() => setIsOpen((is) => !isOpen)}>
+        &times;
+      </button>
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={step >= 1 ? "active" : ""}>1</div>
+            <div className={step >= 2 ? "active" : ""}>2</div>
+            <div className={step >= 3 ? "active" : ""}>3</div>
+          </div>
+
+          <p className="message">
+            Step {step}: {messages[step - 1]}{" "}
+          </p>
+          <div className="buttons">
+            <button className="prev" onClick={handlePrevious}>
+              Previous
+            </button>
+            <button className="next" onClick={handleNext}>
+              Next
+            </button>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
