b24e5bc6d556100b38db4c16ce9eeb973a22ea85
zashaxx <hisenberg2248@gmail.com>
2025-12-01 11:04:58 +0545

    make a new date counter

--- 04-steps/src/App.js
+++ 04-steps/src/App.js
@@ -7,6 +7,15 @@ const messages = [
 ];
 
 export default function App() {
+  return (
+    <div>
+      <Steps />
+      <DateCounter />
+    </div>
+  );
+}
+
+function Steps() {
   function handlePrevious() {
     if (step > 1) setStep((s) => s - 1);
   }
@@ -45,5 +54,43 @@ export default function App() {
         </div>
       )}
     </>
+  );
+}
+
+function DateCounter() {
+  const [step, setStep] = useState(1);
+  const [count, setCount] = useState(0);
+  const date = new Date();
+  date.setDate(date.getDate() + count);
+
+  return (
+    <div className="date-counter">
+
+    <div>
+        <button onClick={() => setStep(step - 5)}>-5</button>
+        <span> Step: {step}</span>
+        <button onClick={() => setStep(step + 5)}>+5</button>
+    </div>
+
+
+      <div>
+        <button onClick={() => setCount(count - step)}>-{step}</button>
+        <span> Count: {count}</span>
+        <button onClick={() => setCount(count + step)}>+{step}</button>
+      </div>
+      <p>
+        <span>
+          {count === 0
+            ? "Today is "
+            : count > 0
+            ? `${count} days from today is: `
+            : `${Math.abs(count)} days from today was: `}
+        </span>
+        <span>
+          {date.toDateString()}
+        </span>
+
+      </p>
+    </div>
   );
 }
