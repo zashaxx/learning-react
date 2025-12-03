3cf7d8cf58c17cee672f634f2105cb5fcee97802
zashaxx <hisenberg2248@gmail.com>
2025-12-03 20:12:14 +0545

    make friend list and button

--- 06-eat-and -split/src/App.js
+++ 06-eat-and -split/src/App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import { useState } from "react";
+
 const initialFriends = [
   {
     id: 118836,
@@ -18,3 +21,46 @@ const initialFriends = [
     balance: 0,
   },
 ];
+
+export default function App() {
+  return (
+    <div className="app">
+      {" "}
+      <div className="sidebar">
+        <FriendList />
+      </div>
+    </div>
+  );
+}
+
+function FriendList() {
+  const friends = initialFriends;
+  return (
+    <ul>
+      {friends.map((friend) => (
+        <Friend friend={friend} key={friend.id} />
+      ))}
+    </ul>
+  );
+}
+
+function Friend({ friend }) {
+  return (
+    <li>
+      <img src={friend.image} alt={friend.name} />
+      <h3>{friend.name}</h3>
+
+      {friend.balance < 0 ? (
+        <p className="red">
+          Your owe {friend.name} ${Math.abs(friend.balance)}
+        </p>
+      ) : friend.balance > 0 ? (
+        <p className="green">
+          {friend.name} owe you ${friend.balance}
+        </p>
+      ) : (
+        <p className="gray">You and {friend.name} are even</p>
+      )}
+    </li>
+  );
+}
