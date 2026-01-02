18d373df5335a4ef637b9a860d05a9bfc9ce333f
zashaxx <hisenberg2248@gmail.com>
2026-01-02 10:26:57 +0545

    feat: update challenges and movie review apps, add .gitignore

--- 07-movie-review/src/index.js
+++ 07-movie-review/src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+// import './index.css';
+// import App from './App';
+import StarRating from './starRating';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <StarRating rating={5} messages={["Dogshit", "Terrible","Good","Excellent","Masterpiece"]} />
+    <StarRating rating={3} color="red" size={48} />
   </React.StrictMode>
 );
 
